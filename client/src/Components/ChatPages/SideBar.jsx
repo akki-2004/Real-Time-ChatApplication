726869556b42c5975e0b31a8594a97d7c3c5e2d8
Tirumani Satya Akshay <147373231+akki-2004@users.noreply.github.com>
2024-10-09 23:06:59 +0530

    searchBar

--- client/src/Components/ChatPages/SideBar.jsx
+++ client/src/Components/ChatPages/SideBar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Box, Button, Text, Tooltip } from '@chakra-ui/react';
+import React, { useState } from 'react'
 
 export default function SideBar() {
+  const [search,setSearch]=useState("");
+  const [loading,setLoading]=useState(false);
+  const [loadingChat,setLoadingChat]=useState(false);
+  const [searchResult,setSearchResult]=useState([]);
+
   return (
-    <div>
-      <h1>side bar</h1>
-    </div>
+    <>
+      <Box
+      d="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      bg="grey"
+      W="100%"
+      p="5px 10px 5px 10px"
+      borderWidth="5px">
+        <Tooltip label="Search users to chat!" hasArrow placement='bottom-end'>
+          <Button variant={"ghost"}>
+            <Text d={{base:"none", md:"flex"}} px="4">Search User</Text>
+            <i class="fas fa-search"></i>
+          </Button>
+        </Tooltip>
+        <Text fontSize={"2xl"} fontFamily={"Afacad Flux"}>Chattify
+          
+        </Text>
+      </Box>
+    </>
   )
 }
